ef6b3c0a5c8e64c6d7fddddf5060a19d8dec56eb
unknown <sivamoorthi151h@gmail.com>
2026-07-26 17:32:20 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+import com.example.demo.repository.UserRepository;
+import com.example.demo.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+}
